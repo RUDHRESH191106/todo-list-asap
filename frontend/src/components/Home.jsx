25cbaedf7dfd9a6c5562102d034d239bb2f4822d
RUDHRESH M <rudhreshm191106@gmail.com>
2025-04-08 10:33:20 +0530

    Updated the files

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import '../App.css'
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+  const newTasks = tasks.filter((_, i)=>i !== index);
+  setTasks(newTasks);
+  };
+  return(
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e)=>setTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={()=>removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
